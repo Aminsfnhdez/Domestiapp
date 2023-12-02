2a1eff966e3f011e15d78e82373469936ddd1515
Aminsfnhdez <digimonataque@gmail.com>
2023-12-01 19:45:17 -0500

    Manual de Usuario

--- src/app/Components/sitemap/sitemap.component.ts
+++ src/app/Components/sitemap/sitemap.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./sitemap.component.css']
 })
 export class SitemapComponent {
-
+  index = false;
+  type = false;
+  imgSrc1 ="../../../assets/images/Manual-0001.jpg";
+  imgSrc2 = "../../../assets/images/Manual-0002.png";
+  imgSrc3 = "../../../assets/images/Manual-0003.jpg";
+  imgSrc4 = "../../../assets/images/Manual-0004.jpg";
+  imgSrc5 = "../../../assets/images/Manual-0005.jpg";
+  imgSrc6 = "../../../assets/images/Manual-0006.jpg";
+  imgSrc7 = "../../../assets/images/Manual-0007.jpg";
 }
